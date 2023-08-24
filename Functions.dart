79965d7475882b349ca54be60fee716fdaed5c7f
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-08-24 16:15:27 +0700

    ADD: Function

---
+++ Functions.dart
@@ -0,0 +1,11 @@
+void main() {
+  int fibonacci(int n) {
+    // print(n);
+    if (n == 0 || n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  var result = fibonacci(20);
+
+  print(result);
+}
