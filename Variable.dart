12abc38822dbae939662f221c6d0c28d24e74b4b
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-08-24 15:50:51 +0700

    ADD: First Code

---
+++ Variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  var name = 'Rohesa Sidiq Permana';
+  var year = 2005;
+  var antennaDiameter = 3.7;
+  var flyByObject = ['Earth', 'Jupiter', 'Mars'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/sarturn.jpg'
+  };
+
+  print(image['tags']);
+}
