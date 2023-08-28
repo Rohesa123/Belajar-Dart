527442ebfaeff1a1e5d5b2e0dae9861b95b5b4d2
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-08-28 09:49:12 +0700

    UPDATE: update Async

--- Async.dart
+++ Async.dart
@@ -1,6 +1,23 @@
 import 'dart:async';
 import 'dart:io';
 
+Future<void> createDescriptions(Iterable<String> objects) async {
+  for (final object in objects) {
+    try {
+      var file = File('file/Async/$object.txt');
+      if (await file.exists()) {
+        var modified = await file.lastModified();
+        print('File for $object already exists. It was modified on $modified.');
+        continue;
+      }
+      await file.create();
+      await file.writeAsString('Start describing $object in this file.');
+    } on IOException catch (e) {
+      print('Cannot create description for $object: $e');
+    }
+  }
+}
+
 void main() {
   const oneSecond = Duration(seconds: 1);
   int i = 1;
@@ -14,4 +31,8 @@ void main() {
   }
 
   printWithDelay("$i Detik");
+
+  var objects = ['object1', 'object2', 'object3'];
+
+  createDescriptions(objects);
 }
